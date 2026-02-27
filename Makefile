f8937f4ae834fc23983e4eef5ed2e71747a03fc7
Dominux <kir.gimranov@gmail.com>
2026-02-27 17:44:43 +0300

    Made summarizer work

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 run:
 	cd services/web && go run ./cmd/main.go
+
+up:
+	docker compose up -d --build --force-recreate --remove-orphans
+
+down:
+	docker compose down
---
+++ services/summarizer/run.sh
@@ -0,0 +1,16 @@
+# Downloading model files
+if [ -d "/models/model.gguf" ]; then
+  echo "Model file already exists"
+else
+  echo "Downloading model"
+
+  wget -O /models/model.gguf https://huggingface.co/bartowski/Qwen2.5-7B-Instruct-GGUF/resolve/main/Qwen2.5-7B-Instruct-Q5_K_M.gguf
+
+  echo "Successfully downloaded model"
+fi
+
+# creating models in background
+sh -c "sleep 5 && ollama create "qwen-zoomer" -f ./Modelfile" &
+
+# Running Ollama
+/bin/ollama serve
